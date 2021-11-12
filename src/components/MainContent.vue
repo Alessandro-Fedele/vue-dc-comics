f757af31f5888360aaa720b3b0702bd4ecb7f811
Alessandro-Fedele <al.fedele@outlook.it>
2021-11-12 18:32:00 +0100

    dist - push

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,24 +1,64 @@
 <template>
   <main class="container">
-    <div>--&#62; Content goes here &#60;--</div>
+    <img src="../assets/jumbotron.jpg" alt="dc-hero" />
+    <div class="current-series">
+      <button>Current Series</button>
+    </div>
+    <ComicCard></ComicCard>
+    <div class="btn-container">
+      <button>Load More</button>
+    </div>
   </main>
 </template>
 
 <script>
+import ComicCard from "@/components/ComicCard.vue";
 export default {
   name: "MainContent",
+  components: {
+    ComicCard,
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .container {
   background-color: #1c1c1c;
-  div {
-    font-size: 30px;
-    color: white;
-    width: 80%;
-    margin: auto;
-    padding: 40px 0;
+  img {
+    width: 100%;
+    object-fit: cover;
+    height: 400px;
+    object-position: top;
+  }
+  .current-series {
+    position: relative;
+    button {
+      position: absolute;
+      top: -25px;
+      left: 170px;
+      color: white;
+      font-size: 25px;
+      background-color: #0282f9;
+      border: 3px solid #0282f9;
+      font-weight: bolder;
+      padding: 5px 15px;
+    }
+  }
+  .btn-container {
+    text-align: center;
+    padding-bottom: 30px;
+    button {
+      color: white;
+      font-size: 15px;
+      background-color: #0282f9;
+      border: 3px solid #0282f9;
+      font-weight: bolder;
+      padding: 8px 50px;
+      margin-top: 60px;
+      &:hover {
+        border: 3px solid white;
+      }
+    }
   }
 }
 </style>
